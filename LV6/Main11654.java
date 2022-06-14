02f05f9b5614da14f1a52d7c4f3813f5cfc19c55
pentonny <98071871+pentonny@users.noreply.github.com>
2022-06-14 16:07:24 +0900

    아스키코드
    charAt(0)

---
+++ LV6/Main11654.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int ch = sc.next().charAt(0);
+		
+		System.out.print(ch);
+	}
+
+}
