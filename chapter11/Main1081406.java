42951072ce484ace09a04714318a1c3a0a5ea9d2
pentonny <wlghd803@naver.com>
2022-07-18 16:27:59 +0900

    나이 순 정렬[StringBuilder[] + BufferedReader + StringBuilder]
    온라인 저지에 가입한 사람들의 나이와 이름이 가입한 순서대로 주어진다. 이때, 회원들을 나이가 증가하는 순으로, 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬하는 프로그램을 작성하시오.

---
+++ chapter11/Main1081406.java
@@ -0,0 +1,34 @@
+package Main001;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main1081406 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		// 입력되는 나이의 범위:1~200;
+		StringBuilder[] p = new StringBuilder[201];
+
+		for (int i = 0; i < p.length; i++) {
+			p[i] = new StringBuilder();
+		}
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int age = Integer.parseInt(st.nextToken());
+			String name = st.nextToken();
+			p[age].append(age).append(' ').append(name).append('\n');
+		}
+		StringBuilder sb = new StringBuilder();
+		for (StringBuilder val : p) {
+			sb.append(val);
+		}
+		System.out.println(sb);
+	}
+
+}
