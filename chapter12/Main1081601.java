3aea8b97d482ce82a3a2a71cdfb8dea7ee96a623
pentonny <wlghd803@naver.com>
2022-07-19 17:09:38 +0900

    숫자카드2[BufferedReader + StringBuilder]
    숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
    10
    6 3 2 10 10 10 -10 -10 7 3
    8
    10 9 -5 2 3 4 5 -10
    =============
    3 0 0 1 2 0 0 2

---
+++ chapter12/Main1081601.java
@@ -0,0 +1,69 @@
+package Main12;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main1081601 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		int M = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < M; i++) {
+			int key = Integer.parseInt(st.nextToken());
+			// upperBOound와 lowerBound의 차이 값을 구한다
+			sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
+		}
+		System.out.println(sb);
+	}
+
+	public static int lowerBound(int[] arr, int key) {
+		int lo = 0;
+		int hi = arr.length;
+
+		while (lo < hi) {
+			int mid = (lo + hi) / 2;
+
+			if (key <= arr[mid]) {
+				hi = mid;
+			} else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	public static int upperBound(int[] arr, int key) {
+		int lo = 0;
+		int hi = arr.length;
+
+		while (lo < hi) {
+			int mid = (lo + hi) / 2;
+
+			if (key < arr[mid]) {
+				hi = mid;
+			} else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+}
