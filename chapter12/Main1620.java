c2e7df664e1e8d50a2eb20747d2c9982b09eaf03
pentonny <wlghd803@naver.com>
2022-07-19 15:39:07 +0900

    포켓몬 마스터(BufferedReader,IOException,InputStreamReader,HashMap,StringTokenizer)
    26 5
    Bulbasaur
    Ivysaur
    Venusaur
    Charmander
    Charmeleon
    Charizard
    Squirtle
    Wartortle
    Blastoise
    Caterpie
    Metapod
    Butterfree
    Weedle
    Kakuna
    Beedrill
    Pidgey
    Pidgeotto
    Pidgeot
    Rattata
    Raticate
    Spearow
    Fearow
    Ekans
    Arbok
    Pikachu
    Raichu
    25
    Raichu
    3
    Pidgey
    Kakuna
    =========
    Pikachu
    26
    Venusaur
    16
    14

---
+++ chapter12/Main1620.java
@@ -0,0 +1,49 @@
+package Main12;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+public class Main1620 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		HashMap<String, Integer> nameMap = new HashMap<String, Integer>();
+		String[] nameArr = new String[N + 1];
+		StringBuilder sb = new StringBuilder();
+		//입력
+		for(int i=1;i<N+1;i++) {
+			String name = br.readLine();
+			nameMap.put(name, i);
+			nameArr[i] = name;
+		}
+		while(M --> 0) {
+			String findStr = br.readLine();
+			if(isStringNumber(findStr)) {
+				int index = Integer.parseInt(findStr);
+				sb.append(nameArr[index]);
+			}
+			else {
+				sb.append(nameMap.get(findStr));
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	public static boolean isStringNumber(String s) {
+		try {
+			Double.parseDouble(s);
+			return true;
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+}
