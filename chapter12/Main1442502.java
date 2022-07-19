7e3c4d6772fba98bbe22299746a1185c86c27bd2
pentonny <wlghd803@naver.com>
2022-07-19 14:50:18 +0900

    문자열 집합(IOException,InputStreamReader,HashSet,Set,StringTokenizer)
    총 N개의 문자열로 이루어진 집합 S가 주어진다.
    입력으로 주어지는 M개의 문자열 중에서 집합 S에 포함되어 있는 것이 총 몇 개인지 구하는 프로그램을 작성하시오.
    5 11
    baekjoononlinejudge
    startlink
    codeplus
    sundaycoding
    codingsh
    baekjoon
    codeplus
    codeminus
    startlink
    starlink
    sundaycoding
    codingsh
    codinghs
    sondaycoding
    startrink
    icerink
    ----------
    4

---
+++ chapter12/Main1442502.java
@@ -0,0 +1,27 @@
+package Main12;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+public class Main1442502 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		HashSet<String> set =new HashSet<>();
+		for(int i=0;i<N;i++) {
+			set.add(br.readLine());
+		}
+		int count =0;
+		for(int i=0;i<M;i++) {
+			if(set.contains(br.readLine())) count++;
+		}
+		System.out.println(count);
+	} 
+
+}
