bc5e869d7bb8fd8d1562f1dda6050176918e48f1
pentonny <wlghd803@naver.com>
2022-07-21 17:37:04 +0900

    네 번째 점(BufferedReader,split)
    세 점이 주어졌을 때, 축에 평행한 직사각형을 만들기 위해서 필요한 네 번째 점을 찾는 프로그램을 작성하시오.
    5 5
    5 7
    7 5
    ===
    7 7

---
+++ chapter13/Main300902.java
@@ -0,0 +1,45 @@
+package Main13;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main300902 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] coord_1 = br.readLine().split(" ");
+		String[] coord_2 = br.readLine().split(" ");
+		String[] coord_3 = br.readLine().split(" ");
+		String x,y;
+		
+		//x 좌표 비교후 쌍을 이루지 않는 x좌표를 저장
+		//1번 x좌표와 2번 x좌표 비교
+		if(coord_1[0] == coord_2[0]) {
+			x = coord_3[0];
+		}
+		//1번 x좌표와 3번 x좌표 비교
+		else if(coord_1[0] == coord_3[0]){
+			x = coord_2[0];
+		}
+		//2번 x좌표와 3번의 x좌표 비교
+		else {
+			x = coord_1[0];
+		}
+		//y 좌표 비교
+		//1번 y좌표와 2번 y좌표 비교
+		if(coord_1[1] == coord_2[1]) {
+			y = coord_3[1];
+		}
+		//1번 y좌표와 3번 y좌표 비교
+		else if(coord_1[1] == coord_3[1]) {
+			y= coord_2[1];
+		}
+		//2번 y좌표와 3번 y좌표 비교
+		else {
+			y = coord_1[1];
+		}
+		System.out.println(x + " " + y);
+	}
+
+}
