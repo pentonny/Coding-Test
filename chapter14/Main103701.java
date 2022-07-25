efe66f973359ad38635b2efc1bf96596aea22101
pentonny <wlghd803@naver.com>
2022-07-25 15:36:51 +0900

    약수(BufferedReader)
    양수 A가 N의 진짜 약수가 되려면, N이 A의 배수이고, A가 1과 N이 아니어야 한다. 어떤 수 N의 진짜 약수가 모두 주어질 때, N을 구하는 프로그램을 작성하시오.

---
+++ chapter14/Main103701.java
@@ -0,0 +1,29 @@
+package Main14;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main103701 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		while (T-- > 0) {
+			int N = Integer.parseInt(st.nextToken());
+			max = N > max ? N : max;
+			min = N < min ? N : min;
+		}
+		System.out.println(max * min);
+	}
+
+}
