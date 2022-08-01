b4cef8f2463ea8d35ff2043ec3f49a5e7f5e846c
pentonny <wlghd803@naver.com>
2022-08-01 15:32:23 +0900

    이항계수1 (알고리즘3)
    자연수 N과 정수 K가 주어졌을 때 이항 계수 
    \(\binom{N}{K}\)를 구하는 프로그램을 작성하시오.
    5 2
    ==
    10

---
+++ chapter14/Main1105002.java
@@ -0,0 +1,34 @@
+package Main14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1105002 {
+	static int[][] dp;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		dp = new int[N + 1][K + 1];
+
+		System.out.println(BC(N, K));
+
+	}
+
+	static int BC(int N, int K) {
+		if (dp[N][K] > 0) {
+			return dp[N][K];
+		}
+		if (N == K || K == 0) {
+			return dp[N][K] = 1;
+		}
+		return dp[N][K] = BC(N - 1, K - 1) + BC(N - 1, K);
+	}
+
+}
