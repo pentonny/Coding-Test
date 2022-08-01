8941d2a1217f81e63b39a9e8f5fcccb83287df75
pentonny <wlghd803@naver.com>
2022-08-01 17:16:07 +0900

    다리 놀기(Scanner + 반복문)
    재원이는 서쪽의 사이트와 동쪽의 사이트를 다리로 연결하려고 한다. (이때 한 사이트에는 최대 한 개의 다리만 연결될 수 있다.) 재원이는 다리를 최대한 많이 지으려고 하기 때문에 서쪽의 사이트 개수만큼 (N개) 다리를 지으려고 한다. 다리끼리는 서로 겹쳐질 수 없다고 할 때 다리를 지을 수 있는 경우의 수를 구하는 프로그램을 작성하라.
    3
    2 2
    1 5
    13 29
    ====
    1
    5
    67863915

---
+++ chapter14/Main101001.java
@@ -0,0 +1,37 @@
+package Main14;
+
+import java.util.Scanner;
+
+public class Main101001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int[][] dp = new int[30][30];
+
+		for (int i = 0; i < 30; i++) {
+			dp[i][i] = 1;
+			dp[i][0] = 1;
+		}
+
+		for (int i = 2; i < 30; i++) {
+			for (int j = 1; j < 30; j++) {
+				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+			}
+		}
+
+		int T = sc.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < T; i++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+
+			sb.append(dp[M][N]).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
