0edc730a8396b251a25d68ae2e9c2e7510eee69d
pentonny <wlghd803@naver.com>
2022-08-02 17:53:32 +0900

    N과 M(3) (Scanner + StringBuilder)
    자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
    1부터 N까지 자연수 중에서 M개를 고른 수열
    같은 수를 여러 번 골라도 된다.
    4 2
    ===
    1 1
    1 2
    1 3
    1 4
    2 1
    2 2
    2 3
    2 4
    3 1
    3 2
    3 3
    3 4
    4 1
    4 2
    4 3
    4 4

---
+++ chapter15/Main15651.java
@@ -0,0 +1,36 @@
+package Main15;
+
+import java.util.Scanner;
+
+public class Main15651 {
+	public static int[] arr;
+	public static int N, M;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[M];
+		dfs(0);
+		System.out.print(sb);
+
+	}
+
+	public static void dfs(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i] + " ");
+			}
+			sb.append('\n');
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			arr[depth] = i;
+			dfs(depth + 1);
+		}
+	}
+
+}
