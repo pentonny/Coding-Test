b740b7bec318f0625851e8a585c27e19d39a6995
pentonny <wlghd803@naver.com>
2022-08-02 17:34:46 +0900

    N과 M(2) (Scanner)
    자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
    1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
    고른 수열은 오름차순이어야 한다.
    4 2
    ==
    1 2
    1 3
    1 4
    2 3
    2 4
    3 4

---
+++ chapter15/Main15650.java
@@ -0,0 +1,34 @@
+package Main15;
+
+import java.util.Scanner;
+
+public class Main15650 {
+	public static int[] arr;
+	public static int N, M;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[M];
+
+		dfs(1, 0);
+	}
+
+	public static void dfs(int at, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = at; i <= N; i++) {
+			arr[depth] = i;
+			dfs(i + 1, depth + 1);
+		}
+	}
+
+}
