ee91c8dc88c966bc2260be0c246bbe328fabd599
pentonny <wlghd803@naver.com>
2022-08-02 16:31:11 +0900

    N과 M(1) (BufferedReader + StringBuilder + N과 M 전정변수화)
    자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
    1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
    4 2
    ===
    1 2
    1 3
    1 4
    2 1
    2 3
    2 4
    3 1
    3 2
    3 4
    4 1
    4 2
    4 3

---
+++ chapter15/Main1564903.java
@@ -0,0 +1,49 @@
+package Main15;
+
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main1564903 {
+	public static int N;
+	public static int M;
+	public static int[] arr;
+	public static boolean[] visit;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		visit = new boolean[N];
+		
+		dfs(0);
+		System.out.println(sb);
+
+	}
+
+	public static void dfs(int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				arr[depth] = i + 1;
+				dfs(depth + 1);
+				visit[i] = false;
+			}
+		}
+	}
+}
